b1acff0d7ff1f6db4932d23e3b89ba21d3b37e2b
kirill <kinikitin2@gmail.com>
2024-02-16 16:52:58 +0900

    ff

--- src/ChairPannel.js
+++ src/ChairPannel.js
@@ -5,15 +5,20 @@ import { Grid, Paper } from '@mui/material';
 import { ResponsiveContainer } from 'recharts';
 
 
-const cellColors = { smallW: '#0000FF', avarageW: '#00FF00', bigW: '#FF0000' }
+var style = getComputedStyle(document.body)
+const cellColors = {
+    smallW: style.getPropertyValue('--smallW-color'),
+    avarageW: style.getPropertyValue('--avarageW-color'),
+    bigW: style.getPropertyValue('--bigW-color')
+}
 let data = { weightPerPercent: 1, weights: [1, 1, 1, 1, 1, 11, 1, 1, 1, 1] }
 
-
+let margin = 6
 const CellItem = styled(Paper)(({ color, theme }) => ({
     backgroundColor: color,
-    borderRadius: '90%',
-    height: '80%',
-    padding :'2'
+    borderRadius: '100%',
+    margin: `${margin}%`,
+    height: `${100 - 2 * margin}%`,
 }))
 
 function PannelCel(weight) {
@@ -48,7 +53,7 @@ function PannelRow(weights) {
                     )
                 }
                 return (
-                    <Grid item xs={3} key={key}>
+                    <Grid item style={{ alignItems: 'stretch' }} xs={3} key={key}>
                         {cell_component}
                     </Grid>
                 )
@@ -59,8 +64,8 @@ function PannelRow(weights) {
 
 function ChairPannel() {
     return (
-        <ResponsiveContainer width='100%' aspect={1.0 / 0.4}>
-            <Grid container spacing={1} style={{ padding: 10}}>
+        <ResponsiveContainer width='100%' aspect={4.5 / 2.0}>
+            <Grid container spacing={1} style={{ padding: 10 }}>
                 <PannelRow weights={data.weights.slice(0, 2)} />
                 <PannelRow weights={data.weights.slice(2, 6)} />
                 <PannelRow weights={data.weights.slice(6, 10)} />
